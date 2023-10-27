c0aa166737d7893dcc09836ae7a8ea281481b658
availe <94802525+availe@users.noreply.github.com>
2023-10-27 19:32:51 -0400

    Delete src directory

--- src/Core/Main.java
+++
@@ -1,8 +0,0 @@
-package Core;
-
-public class Main {
-    public static void main(String[] args) {
-        PluginManager.getInstance().loadPlugins("src/plugins/");
-        PluginManager.getInstance().executePlugins();
-    }
-}
--- src/ModuleLogic/Module.java
+++
@@ -1,9 +0,0 @@
-package ModuleLogic;
-
-import java.util.List;
-
-public interface Module {
-    void execute();
-    List<String> getDependencies();
-    boolean isSelfContained();
-}
