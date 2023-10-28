24ba55b33fb256e45bf34fdbe8165382f67de083
availe <94802525+availe@users.noreply.github.com>
2023-10-27 21:59:51 -0400

    Delete src directory

--- src/main/java/Core/Main.java
+++
@@ -1,8 +0,0 @@
-package Core;
-
-public class Main {
-    public static void main(String[] args) {
-        PluginManager.getInstance().loadPlugins("src/main/java/plugins/");
-        PluginManager.getInstance().executePlugins();
-    }
-}
